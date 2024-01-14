81ef9e7915ad72d15b0cb18762e37ff2f5f119b1
Torsman <144048985+Torsman@users.noreply.github.com>
2024-01-14 15:57:48 +0200

    Add files via upload

---
+++ src/components/day/event/index.js
@@ -0,0 +1,81 @@
+import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
+import { useContext } from 'react';
+import CalendarContext from '../../../context/calendar.context';
+
+/**
+ * @param {Object} props
+ * @param {{ id: string, title: string, description: string, date: string }} props.event
+ * @returns
+ */
+const EventComponent = (props) => {
+  const { title, date, description, id } = props.event;
+  const { removeEvent: contextRemoveEvent, setCreateEvent } = useContext(CalendarContext);
+
+
+  const click = (event) => {
+    event.stopPropagation();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    setCreateEvent({
+      id,
+      date,
+      title,
+      description,
+      clientX: event.clientX,
+      clientY: event.clientY,
+    });
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const removeEvent = () => {
+ 
+    contextRemoveEvent(props.event);
+  };
+
+  return (
+    <div className='event-wrapper' onClick={click}>
+      {}
+      <FontAwesomeIcon icon={faClose} id='event-close' onClick={removeEvent} />
+      {}
+      {title}
+    </div>
+  );
+};
+
+export default EventComponent;
